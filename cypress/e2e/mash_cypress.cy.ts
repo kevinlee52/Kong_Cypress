b29bef0216b2f62f80b52d0a3edb385e2ac0197f
Kevin Lee <kevinlee52@163.com>
2024-07-29 18:40:50 +0800

    init

---
+++ cypress/e2e/mash_cypress.cy.ts
@@ -0,0 +1,21 @@
+describe("create a new service", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("information", () => {
+    cy.get("[data-test='hero-heading']").contains(
+      "Find in-depth information about Next.js features and API."
+    );
+  });
+
+  // it.only("the features on the homepage are correct", () => {
+  //   cy.get("dt")
+  //     .eq(0)
+  //     .contains(/4 courses/i)
+  //   cy.get("dt").eq(1).contains("25+ Lessons")
+  //   cy.get("dt")
+  //     .eq(2)
+  //     .contains(/free and open/i)
+  // })
+});
